e74fb44e0f613e15f4ebb7fef6e612a4bd3ecc31
Jason Lieb <Jason.lieb@outlook.com>
2023-03-26 22:12:25 -0400

    add background with more contrast to h1

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,7 +4,9 @@ export default function Nav({ selectedPage, setPage, setTimePeriod, setData }) {
   const pages = ['Stocks', 'Currency', 'Government Data']
   return (
     <nav className="navbar navbar-expand-md border-bottom border-dark border-opacity-50 bg-body-tertiary justify-content-between">
-      <h1 className="text-success ms-2 m-0 p-2 fw-bolder rounded fs-3">Stock Market Visualizer</h1>
+      <div className="">
+        <h1 className="text-success ms-2 m-0 p-2 fw-bolder rounded fs-3 title">Stock Market Visualizer</h1>
+      </div>
       <button
         className="navbar-toggler me-2"
         type="button"
